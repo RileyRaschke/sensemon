8f22c7f10ecaac2c3e34251b1a777dc62b650e40
Riley Raschke <riley@rrappsdev.com>
2023-06-21 07:34:10 -0500

    cleanup table create

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,13 +1,6 @@
 
-drop table sensemon.sensorreads;
-/*
-create table sensemon.sensorreads (
-   sr_date date,
-   sr_device_id varchar2(30 char),
-   sr_farenheit number,
-   sr_humidity number
-);
-*/
+--drop table sensemon.sensorreads;
+
 create table sensemon.sensorreads (
    sr_date date,
    sr_device_id varchar2(30 char),
